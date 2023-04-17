2319c68c63ff7e429bdaa7f7772997c99213811e
Dana Scheider <dana@airrobe.com>
2023-04-18 09:25:38 +1000

    WIP

--- cartridges/int_airrobe_core/cartridge/scripts/helpers/airrobeMultiOptInHelpers.js
+++ cartridges/int_airrobe_core/cartridge/scripts/helpers/airrobeMultiOptInHelpers.js
@@ -5,22 +5,30 @@
 function getAirrobeMultiOptInProps() {
   const collections = require('*/cartridge/scripts/util/collections')
   const getCategory = require('*/cartridge/scripts/util/getCategory')
+  const ProductLineItemsModel = require('*/cartridge/models/productLineItems')
   const BasketMgr = require('dw/order/BasketMgr')
   const currentBasket = BasketMgr.getCurrentBasket()
-  if (currentBasket == null) {
-    return {}
-  }
 
-  const categories = collections.map(currentBasket.getAllProductLineItems(), function (lineItem) {
-    let productId = lineItem.getProduct().getID()
-    let category = getCategory(null, productId, [])
+  if (currentBasket == null) return []
+
+  // const categories = collections.map(currentBasket.getAllProductLineItems(), function (lineItem) {
+  //   let productId = lineItem.getProduct().getID()
+  //   let category = getCategory(null, productId, [])
+
+  //   return { category }
+  // })
+
+  // TODO: How to put the categories in the correct product object?
+
+  const lineItems = new ProductLineItemsModel(currentBasket.productLineItems, 'basket')
+
+  const categories = lineItems.items.map((product) => {
+    const category = getCategory(null, product.id, [])
 
     return { category }
   })
 
-  return {
-    categories,
-  }
+  return categories || []
 }
 
 module.exports = {
